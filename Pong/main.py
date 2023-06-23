b2b8b64b7ee68d7a58080e2d432c2a6591225d67
Oskar <oskar.wichtowski3@gmail.com>
2023-06-24 00:18:14 +0200

    initial commit

--- Pong/main.py
+++ Pong/main.py
@@ -3,6 +3,9 @@ import pickle
 import pygame
 import neat
 import time
+
+from neat import Population
+
 from pong import Game
 
 
@@ -22,8 +25,9 @@ class PongGame:
         run = True
 
         while run:
+            # FPS limiter
             clock.tick(60)
-
+            # Przechwytywanie eventów
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
@@ -40,9 +44,10 @@ class PongGame:
             self.game.draw(draw_score=True)
             pygame.display.update()
 
-    def train_ai(self, g1, g2, cfg, draw=False):
+    def train_single_ai_pair(self, g1, g2, cfg, draw):
         # Trenowanie SI przy użyciu dwóch genomów NEAT i konfiguracji
         start_time = time.time()
+        # FeedForward nie jest rekurencyjną siecią co oznacza że nie ma pamięci wstecznej
         n1 = neat.nn.FeedForwardNetwork.create(g1, cfg)
         n2 = neat.nn.FeedForwardNetwork.create(g2, cfg)
         self.genome1 = g1
@@ -55,15 +60,16 @@ class PongGame:
                     return True
 
             game_info = self.game.loop()
+            # call move by AI
             self.move_ai_paddles(n1, n2)
 
             if draw:
-                self.game.draw(draw_score=False, draw_hits=True)
+                self.game.draw(draw_score=True, draw_hits=True)
 
             pygame.display.update()
 
             duration = time.time() - start_time
-            if game_info.left_score == 1 or game_info.right_score == 1 or game_info.left_hits >= max_hits:  # max hits == 50
+            if game_info.left_score == 3 or game_info.right_score == 3 or game_info.left_hits >= max_hits:  # max hits == 50
                 self.calculate_fitness(game_info, duration)
                 break
 
@@ -85,37 +91,46 @@ class PongGame:
             else:  # Porusz w górę lub w dół
                 valid = self.game.move_paddle(left=left, up=(decision == 1))
 
-                if not valid:  # Karanie SI, jeśli paletka wychodzi poza ekran
+                if not valid:  # jeśli paletka wychodzi poza ekran
                     genome.fitness -= 1
 
     def calculate_fitness(self, game_info, duration):
         # Obliczanie dokładności genomów na podstawie informacji o grze
+        # Fitness to prościej ujmując wynik. W przypadku poniżej
+        # self.genome1.fitness += game_info.left_score + game_info.left_hits - duration
+        # self.genome2.fitness += game_info.right_score + game_info.right_hits - duration
         self.genome1.fitness += game_info.left_hits + duration
         self.genome2.fitness += game_info.right_hits + duration
+        # pierwsza próba - ostatnie generacje dochodziły do wniosku że czas + ilość uderzeń była
+        # najbardziej korzystna dla obu stron więc odbijały w jedym miejscu oby dwie przez max'a wiecznie
 
 
 def eval_genomes(genomes, cfg):
-    # Ewaluacja genomów NEAT
+    # Ewaluacja genomów
+    # Gen - Node lub connection, reprezentuje różne cechy, takie jak wagi połączeń między neuronami, progi aktywacji
+    # Genom - set Genów
     width, height = 700, 500
     win = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Pong")
-
+    # iterate przez index oraz wartość genomu wszystkich genomów
     for i, (genome_id1, genome1) in enumerate(genomes):
+        # zerowanie przystosowania aby uniknąć nierównych szans podczas
         genome1.fitness = 0
+        # wybieranie podzbioru w populacji, aby każdy miał szanse zagrać z każdym
         for genome_id2, genome2 in genomes[min(i + 1, len(genomes) - 1):]:
+            # wartość_if_true IF warunek ELSE wartość_IF_FALSE
             genome2.fitness = 0 if genome2.fitness is None else genome2.fitness
             pong = PongGame(win, width, height)
-
-            force_quit = pong.train_ai(genome1, genome2, cfg, draw=True)
+            force_quit = pong.train_single_ai_pair(genome1, genome2, cfg, True)
             if force_quit:
                 quit()
 
 
 def test_best(cfg):
-    # Testowanie najlepszej sieci neuronowej na podstawie wcześniej zapisanego najlepszego genomu
+    # Testowanie najlepszej sieci neuronowej na podstawie wcześniej zapisanego pliku python obj
     with open("best.pickle", "rb") as f:
         winner = pickle.load(f)
-
+    # tworzenie sieci z najlepszego genomu
     winner_net = neat.nn.FeedForwardNetwork.create(winner, cfg)
 
     width, height = 700, 500
@@ -127,14 +142,13 @@ def test_best(cfg):
 
 def neat_runner(cfg):
     # checkpoint z którego chcemy startować (jeśli odkomentujemy trzeba zakomentować neat.Population)
-    population = neat.Checkpointer.restore_checkpoint('neat-checkpoint-36')
+    population = neat.Checkpointer.restore_checkpoint('neat-checkpoint-37')
     # population: Population = neat.Population(cfg)
-    population.add_reporter(neat.StdOutReporter(True))  # reporting via console
-    stats = neat.StatisticsReporter()
-    population.add_reporter(stats)
+    population.add_reporter(neat.StdOutReporter(True))  # reporting przez konsole
     population.add_reporter(neat.Checkpointer(1))
 
     winn = population.run(eval_genomes, 50)
+    # eval_genomes jest wywoływana dla każdej generacji populacji, a liczba 50 określa, ile generacji ma być przetwarzanych.
     with open("best.pickle", "wb") as f:
         pickle.dump(winn, f)
 
@@ -142,10 +156,13 @@ def neat_runner(cfg):
 def main():
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config.txt')
+    # https://neat-python.readthedocs.io/en/latest/config_file.html
+    # Neat posiada defaultowe wartości na których sieć neuronowa będzie operować
     cfg = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                       neat.DefaultSpeciesSet, neat.DefaultStagnation,
                       config_path)
     neat_runner(cfg)
+    test_best(cfg)
 
 
 if __name__ == '__main__':
